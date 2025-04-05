69862088a6b60867e4d7d499c9dde928894b0d29
Ashen <mnojasish@gmail.com>
2025-04-05 14:29:01 +0530

    Updated only pika code proto, rest are just bases

--- Pikachu/Core/Src/main.c
+++ Pikachu/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -30,6 +31,18 @@
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
+#define ROW_COUNT    2
+#define COL_COUNT    5
+#define MCU1_ADDRESS  0x01
+#define MCU2_ADDRESS  0x02
+#define MCU3_ADDRESS  0x03
+
+uint16_t rowPins[ROW_COUNT] = {R1_Pin, R2_Pin};
+uint16_t colPins[COL_COUNT] = {C1_Pin, C2_Pin, C3_Pin, C4_Pin, C5_Pin};
+uint8_t keymap[ROW_COUNT][COL_COUNT] = {
+    {0x04, 0x05, 0x06, 0x07, 0x08},
+    {0x0A, 0x0B, 0x0C, 0x0D, 0x0E}
+};
 /* USER CODE BEGIN PD */
 
 /* USER CODE END PD */
@@ -40,6 +53,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
@@ -50,6 +64,10 @@ UART_HandleTypeDef huart1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
+static void UART_Receive_Data(void);
+static void Send_HID_Key(uint8_t key);
+static void scan_keypad(void);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -89,6 +107,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -97,9 +116,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	  scan_keypad();
+	  UART_Receive_Data();
   }
   /* USER CODE END 3 */
 }
@@ -112,14 +130,18 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -129,12 +151,18 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
@@ -186,10 +214,13 @@ static void MX_GPIO_Init(void)
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /*Configure GPIO pins : R1_Pin R2_Pin C1_Pin C2_Pin */
-  GPIO_InitStruct.Pin = R1_Pin|R2_Pin|C1_Pin|C2_Pin;
+  /*Configure GPIO pins : R1_Pin R2_Pin C1_Pin C2_Pin
+                           C3_Pin C4_Pin C5_Pin */
+  GPIO_InitStruct.Pin = R1_Pin|R2_Pin|C1_Pin|C2_Pin
+                          |C3_Pin|C4_Pin|C5_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -197,6 +228,156 @@ static void MX_GPIO_Init(void)
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
+}
+
+void scan_keypad(void) {
+    static uint8_t previous_state[ROW_COUNT][COL_COUNT] = {0};  // Store previous state of keys
+    uint8_t any_key_pressed = 0;  // Flag to track if any key is pressed
+
+    for (int row = 0; row < ROW_COUNT; row++) {
+        HAL_GPIO_WritePin(GPIOA, rowPins[row], GPIO_PIN_SET);  // Set row to high
+
+        for (int col = 0; col < COL_COUNT; col++) {
+            if (HAL_GPIO_ReadPin(GPIOA, colPins[col]) == GPIO_PIN_RESET) {  // Check if key is pressed
+                uint8_t key_code = keymap[row][col];  // Get the corresponding keycode from keymap
+                Send_HID_Key(key_code);  // Send HID report for key press
+            }
+        }
+
+        HAL_GPIO_WritePin(GPIOA, rowPins[row], GPIO_PIN_RESET);  // Reset row to low
+        HAL_Delay(10);  // Debouncing delay
+    }
+    HAL_Delay(100);  // Additional debounce or delay between scans
+}
+
+void Send_HID_Key(uint8_t key) {
+    uint8_t HID_report[8] = {0};  // HID report with a size of 8 bytes
+
+    // Set the key in the HID report (the key code for 'W', 'A', 'S', 'D')
+    HID_report[2] = key;  // Using byte 2 to represent the key pressed
+
+    // Send the HID report (press key)
+    if (hpcd_USB_FS.State == HAL_PCD_STATE_READY) {
+        USBD_HID_SendReport(&hUsbDeviceFS, HID_report, sizeof(HID_report));
+    }
+
+    // Delay to simulate key press duration
+    HAL_Delay(100);
+
+    // Release the key (send 0)
+    HID_report[2] = 0;
+    if (hpcd_USB_FS.State == HAL_PCD_STATE_READY) {
+        USBD_HID_SendReport(&hUsbDeviceFS, HID_report, sizeof(HID_report));
+    }
+}
+
+void UART_Receive_Data(void) {
+	uint8_t received_data[2];
+
+	if (HAL_UART_Receive(&huart1, (uint8_t *)received_data, 2, 100) == HAL_OK) {
+		uint8_t received_address = received_data[0];
+        uint8_t data = received_data[1];
+
+        if (received_address == MCU1_ADDRESS) {
+        	switch (data)
+        	{
+        	case 'Z':
+        		Send_HID_Key(0x1D);
+        		break;
+
+        	case 'X':
+        		Send_HID_Key(0x1B);
+        	    break;
+
+        	case 'C':
+        	    Send_HID_Key(0x06);
+        	    break;
+
+        	case 'V':
+        	    Send_HID_Key(0x19);
+        	    break;
+
+        	case 'M':
+        	    Send_HID_Key(0x2C);
+        	    break;
+
+        	case 'M':
+        		Send_HID_Key(0x2C);
+        	    break;
+
+        	case 'M':
+        	    Send_HID_Key(0x2C);
+        	    break;
+    }
+}
+        if(received_address == MCU2_ADDRESS){
+        	switch(data){
+
+        	case 'Y':
+        		Send_HID_Key(0x1C);
+        	    break;
+
+        	case 'U':
+        		Send_HID_Key(0x18);
+        		break;
+
+        	case 'I':
+        		Send_HID_Key(0x0C);
+        		break;
+
+        	case 'O':
+        		Send_HID_Key(0x12);
+        		break;
+
+        	case 'P':
+        		Send_HID_Key(0x13);
+        		break;
+
+        	case 'H':
+        		Send_HID_Key(0x0B);
+        		break;
+
+        	case 'J':
+        		Send_HID_Key(0x0D);
+        		break;
+
+        	case 'K':
+        		Send_HID_Key(0x0E);
+        		break;
+
+        	case 'L':
+        		Send_HID_Key(0x0F);
+        		break;
+        	}
+}
+        if(received_data == MCU3_ADDRESS){
+        	switch(data){
+
+        	case 'B':
+        		Send_HID_Key(0x05);
+        		break;
+
+        	case 'N':
+        		Send_HID_Key(0x11);
+        		break;
+
+        	case 'M':
+        		Send_HID_Key(0x10);
+        		break;
+
+        	case 'A':
+        		Send_HID_Key(0x2C);
+        		break;
+
+        	case 'A':
+        		Send_HID_Key(0x2C);
+        		break;
+
+        	case 'A':
+        		Send_HID_Key(0x2C);
+        		break;
+        	}
+        }
 }
 
 /* USER CODE BEGIN 4 */
